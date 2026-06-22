6fc027abcd40aeeb1af6809abb71d007a395dcfb
gracy raghav <gracyraghav.04@gmail.com>
2026-06-22 22:10:31 +0530

    modified withsome more classes and objects concepts

--- consndattributes.py
+++ consndattributes.py
@@ -28,3 +28,26 @@ a2 = BankAccount("Rahul", 12000)
 
 a1.show_balance()
 a2.show_balance()
+
+class Employee:
+    company = "Google"#class variable
+
+    def __init__(self, name):
+        self.name = name #instance variable
+
+e1 = Employee("Elly")
+e2 = Employee("Rahul")
+
+print(e1.company)
+print(e2.company)
+print(e1.name)
+print(e2.name)
+class BankAccount:
+    def __init__(self, balance):
+        self.balance = balance
+
+    def show_balance(self): #instance method; works with  the object's data.
+        print("Balance:", self.balance)
+
+a1 = BankAccount(5000)
+a1.show_balance()
