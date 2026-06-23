e7f1779d531cea9e6261f484ada36899db191d12
gracy raghav <gracyraghav.04@gmail.com>
2026-06-23 23:34:36 +0530

    modified super.py

--- super.py
+++ super.py
@@ -10,3 +10,4 @@ class Dog(Animal):
 
 
 d = Dog()
+#Inheritance automatically gives access to parent members. super() is used when the child class wants to explicitly invoke the parent class's implementation (especially constructors and overridden methods) instead of replacing it completely.
