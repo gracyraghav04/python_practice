aab46eb7610e218ebffefd1bf4419ebc2e68fa68
gracy raghav <gracyraghav.04@gmail.com>
2026-06-24 23:53:09 +0530

    polymorphism

---
+++ polymorphism.py
@@ -0,0 +1,14 @@
+class Dog:
+    def sound(self):
+        print("Bark")
+
+
+class Cat:
+    def sound(self):
+        print("Meow")
+
+
+animals = [Dog(), Cat()]
+
+for a in animals:
+    a.sound()
