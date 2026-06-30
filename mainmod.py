f195d86e4eaf777a3b86026e601b3a5e6ce1774f
gracy raghav <gracyraghav.04@gmail.com>
2026-06-30 23:55:01 +0530

    added more concepts learned uptill now and comments for clarity

---
+++ mainmod.py
@@ -0,0 +1,4 @@
+from studentmod import Student
+s=Student("John")
+s.show()
+#import classes,functions,variables from another modules
--- modules.py
+++ modules.py
@@ -2,3 +2,8 @@
 import calculatorasmod #Access calculatorasmod.add()  and to calculatorasmodsubtract()
 print(calculatorasmod.add(10,20))
 print(calculatorasmod.subtract(10,20))
+
+
+
+from math import *# built-in python module
+print(sqrt(19))
